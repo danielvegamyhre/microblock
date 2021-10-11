5d2da195b28ec494a819fb43920411826c079530
malwaredllc <30509968+malwaredllc@users.noreply.github.com>
2021-10-10 19:39:29 -0600

    add precommit

--- microblock/node.py
+++ microblock/node.py
@@ -3,12 +3,12 @@ from microblock import app, local_chain
 from dataclasses import asdict
 
 
-@app.route('/nodes', methods=['GET'])
+@app.route("/nodes", methods=["GET"])
 def get_nodes():
     return jsonify(list(local_chain.nodes)), 200
 
 
-@app.route('/nodes/register', methods=['POST'])
+@app.route("/nodes/register", methods=["POST"])
 def register_nodes():
     if not request.json:
         return "Missing node information in JSON format", 400
@@ -18,45 +18,39 @@ def register_nodes():
         local_chain.add_node(node_address)
 
     response = {
-        'message': f'added {len(nodes)} new nodes',
-        'total_nodes': f'{len(local_chain.nodes)}'
+        "message": f"added {len(nodes)} new nodes",
+        "total_nodes": f"{len(local_chain.nodes)}",
     }
     return jsonify(response), 200
 
 
-@app.route('/chain', methods=['GET'])
+@app.route("/chain", methods=["GET"])
 def get_chain():
     return jsonify(local_chain.chain), 200
 
 
-@app.route('/consensus', methods=['GET'])
+@app.route("/consensus", methods=["GET"])
 def get_longest_chain():
     result = local_chain.consensus()
     if result:
-        response = {
-            'message': 'local chain replaced',
-            'chain': local_chain.chain
-        }
+        response = {"message": "local chain replaced", "chain": local_chain.chain}
     else:
         response = {
-            'message': 'local chain is authoritative',
-            'chain': local_chain.chain
+            "message": "local chain is authoritative",
+            "chain": local_chain.chain,
         }
     return jsonify(response), 200
 
 
-@app.route('/transactions/new', methods=['POST'])
+@app.route("/transactions/new", methods=["POST"])
 def new_transaction():
     if not request.json:
         return "Missing transaction information in JSON format", 400
 
     data = request.get_json()
-    if not all(map(lambda x: x in data, ['sender','recipient','amount'])):
+    if not all(map(lambda x: x in data, ["sender", "recipient", "amount"])):
         return "Missing required fields: ['sender','recipient','amount']", 400
 
     block_index = local_chain.new_transaction(data)
-    res = {
-        'message': f'transaction will be part of block {block_index}'
-    }
+    res = {"message": f"transaction will be part of block {block_index}"}
     return jsonify(res), 200
-    
